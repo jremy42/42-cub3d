d840eac0f8e4730b9938d9d6ced347d3761a9fac
Frederic LE BLAY <fle-blay@e1r2p16.42.fr>
2022-07-04 14:55:59 +0200

    Ajout check map name

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jremy <jremy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 14:35:04 by jremy             #+#    #+#             */
-/*   Updated: 2022/07/04 14:30:45 by jremy            ###   ########.fr       */
+/*   Updated: 2022/07/04 14:54:00 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,10 @@ int	main(int ac, char **av)
 		__putstr_fd("Error\nneed maps\n", 2);
 		exit(1);
 	}
+	if (__strlen(av[1]) < 5
+		|| !__strstr(av[1], ".cub")
+		|| __strcmp(av[1] + __strlen(av[1]) - 4, ".cub"))
+		__putstr_fd("Error\nWrong map name extension\n", 2);
 	parsing(av, &cub);
 	print_cub(&cub);
 	__putstr_fd("Hello Raycasted World\n", 1);
