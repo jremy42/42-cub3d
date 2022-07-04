7fb5d466ac9b706079d8c5281fece6d91f14dda0
Jonathan REMY <jremy@e1r2p17.clusters.42paris.fr>
2022-07-04 18:54:16 +0200

    fix script valgrind

--- test_wrong_maps.sh
+++ test_wrong_maps.sh
@@ -7,7 +7,7 @@ test "$#" -ne 1 && echo "Missing directory" && exit 1
 for i in `ls ${MAP_DIR}`
 do
 	echo "Testing [$MAP_DIR/$i]"
-	OUTPUT=$(./cub3d $MAP_DIR/$i 2>&1)
+	OUTPUT=$(valgrind ./cub3d $MAP_DIR/$i 2>&1)
 	RET=$?
 	echo "$OUTPUT"
 	test "$RET" -lt 128 || echo -e "\e[31mSIGNALED on [$i]\e[0m"
