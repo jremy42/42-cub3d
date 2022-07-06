41cd2ce5e0a9e19acc0dde3f512ffeeac27bc1c8
Deus <deus@MacBook-Pro-de-Deus.local>
2022-07-06 11:00:06 +0200

    add hook

--- srcs/hook.c
+++ srcs/hook.c
@@ -37,15 +37,15 @@ int	hooking(int keycode, t_cub *cub)
 	f_hook[LEFT] = __hookleft;
 	f_hook[RIGHT] = __hookright;
 	
-	if (keycode == 0 || keycode == 123)
+	if (keycode == KEY_L || keycode == 123)
 		f_hook[LEFT](cub);
-	if (keycode == 1 || keycode == 125)
+	if (keycode == KEY_D || keycode == 125)
 		f_hook[DOWN](cub);
-	if (keycode == 2 || keycode == 124)
+	if (keycode == KEY_R || keycode == 124)
 		f_hook[RIGHT](cub);
-	if (keycode == 13 || keycode == 126)
+	if (keycode == KEY_U || keycode == 126)
 		f_hook[UP](cub);
-	if (keycode == 53)
+	if (keycode == KEY_ESC)
 		exit(1);    // <- a modifier
 	return (0);
 }
